56fb756c5e22ce2e5c5252d719400f5633f99f75
Martifas <martynas.grumadas@gmail.com>
2025-02-25 13:15:15 +0200

    channel select fixed

--- src/components/Channels/Channels.jsx
+++ src/components/Channels/Channels.jsx
@@ -1,13 +1,21 @@
 import './Channels.css'
 
-function Channels({ channelList }) {
+function Channels({ channelList, activeChannel, onSelectChannel }) {
   return (
     <div className="channelList">
+      <h2>{activeChannel}</h2>
       {channelList.map((channel, index) => (
-        <a className="channel" key={index}># {channel.name} </a>
+        <a
+          className="channel"
+          onClick={() => onSelectChannel(channel.name)}
+          key={index}
+        >
+          # {channel.name}
+        </a>
       ))}
     </div>
   )
 }
+
 
 export default Channels
